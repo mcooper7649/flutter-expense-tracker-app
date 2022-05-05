110e6321235195617f04f5a0f3c30dec000f8fca
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-05-05 08:40:42 -0400

    broke main dart into smaller components and stateful widgets. Then refactored and lifted state

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import './widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
-import './transaction.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -48,35 +47,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final List<Transaction> transactions = [
-      Transaction(
-        id: 't1',
-        title: 'New Shoes',
-        amount: 69.99,
-        date: DateTime.now(),
-      ),
-      Transaction(
-        id: 't2',
-        title: 'New Hat',
-        amount: 29.99,
-        date: DateTime.now(),
-      )
-    ];
     // String titleInput = '';
     // String amountInput = '';
 
@@ -107,90 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 elevation: 5,
               ),
             ),
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
-                      // onChanged: (val) {
-                      //   titleInput = val;
-                      // },
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-                      // onChanged: (val) {
-                      //   amountInput = val;
-                      // },
-                    ),
-                    FlatButton(
-                      child: Text('Add Transaction'),
-                      // onPressed: () {
-                      //   print(titleInput);
-                      //   print(amountInput);
-                      // },
-                      onPressed: () {
-                        print(titleController.text);
-                        print(amountController.text);
-                      },
-                      textColor: Colors.purple,
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${tx.amount}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Text(
-                            DateFormat.yMMMMd().format(tx.date),
-                            style: TextStyle(
-                                fontWeight: FontWeight.w200,
-                                fontSize: 12,
-                                color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              }).toList(),
-            ),
+            UserTransactions()
           ],
         ));
   }
